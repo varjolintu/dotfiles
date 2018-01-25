352b6a89f73e72cde8bd9c12644a72074159ef47
varjolintu <sami.vanttinen@protonmail.com>
2018-01-25 09:49:47 +0200

    Updated draw script

--- draw.sh
+++ draw.sh
@@ -2,8 +2,8 @@
 read -r X Y W H G ID < <(slop -f "%x %y %w %h %g %i" -b 1 -t 0 -q)
 # Width and Height in px need to be converted to columns/rows
 # To get these magic values, make a fullscreen st, and divide your screen width by ${tput cols}, height by ${tput lines} 
-let "W = W / 6"
-let "H = H / 12"
+let "W = W / 6 - 2"
+let "H = H / 12 - 1"
 g=${W}x${H}+${X}+${Y}
 echo $g #debugging
 urxvt -g $g -name floating
